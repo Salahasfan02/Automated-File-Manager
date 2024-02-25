fce7e8ab4d603fdf896a2a3076c20eafac817497
Salahasfan02 <157410811+Salahasfan02@users.noreply.github.com>
2024-02-25 00:05:24 +0000

    Update Management.py

--- Management.py
+++ Management.py
@@ -8,8 +8,7 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-# ! FILL IN BELOW
-# ? folder to track e.g. Windows: "C:\\Users\\satan\\Downloads"
+
 source_dir = "C:\\Users\\satan\\Downloads"
 dest_dir_sfx = "C:\\Users\\satan\\OneDrive\\Desktop\\Organized\\SFX"
 dest_dir_music = "C:\\Users\\satan\\OneDrive\\Desktop\\Organized\\Music"
@@ -17,15 +16,15 @@ dest_dir_video = "C:\\Users\\satan\\OneDrive\\Desktop\\Organized\\Video"
 dest_dir_image = "C:\\Users\\satan\\OneDrive\\Desktop\\Organized\\Image"
 dest_dir_documents = "C:\\Users\\satan\\OneDrive\\Desktop\\Organized\\Documents"
 
-# ? supported image types
+
 image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
                     ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
-# ? supported Video types
+
 video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
                     ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
-# ? supported Audio types
+
 audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
-# ? supported Document types
+
 document_extensions = [".doc", ".docx", ".odt",
                        ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
 
@@ -33,7 +32,9 @@ document_extensions = [".doc", ".docx", ".odt",
 def make_unique(dest, name):
     filename, extension = splitext(name)
     counter = 1
-    # * IF FILE EXISTS, ADDS NUMBER TO THE END OF THE FILENAME
+
+
+  
     while exists(f"{dest}/{name}"):
         name = f"{filename}({str(counter)}){extension}"
         counter += 1
@@ -51,8 +52,8 @@ def move_file(dest, entry, name):
 
 
 class MoverHandler(FileSystemEventHandler):
-    # ? THIS FUNCTION WILL RUN WHENEVER THERE IS A CHANGE IN "source_dir"
-    # ? .upper is for not missing out on files with uppercase extensions
+
+  
     def on_modified(self, event):
         with scandir(source_dir) as entries:
             for entry in entries:
@@ -62,7 +63,7 @@ class MoverHandler(FileSystemEventHandler):
                 self.check_image_files(entry, name)
                 self.check_document_files(entry, name)
 
-    def check_audio_files(self, entry, name):  # * Checks all Audio Files
+    def check_audio_files(self, entry, name):  
         for audio_extension in audio_extensions:
             if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
                 if entry.stat().st_size < 10_000_000 or "SFX" in name:  # ? 10Megabytes
@@ -72,26 +73,26 @@ class MoverHandler(FileSystemEventHandler):
                 move_file(dest, entry, name)
                 logging.info(f"Moved audio file: {name}")
 
-    def check_video_files(self, entry, name):  # * Checks all Video Files
+    def check_video_files(self, entry, name):  
         for video_extension in video_extensions:
             if name.endswith(video_extension) or name.endswith(video_extension.upper()):
                 move_file(dest_dir_video, entry, name)
                 logging.info(f"Moved video file: {name}")
 
-    def check_image_files(self, entry, name):  # * Checks all Image Files
+    def check_image_files(self, entry, name):  
         for image_extension in image_extensions:
             if name.endswith(image_extension) or name.endswith(image_extension.upper()):
                 move_file(dest_dir_image, entry, name)
                 logging.info(f"Moved image file: {name}")
 
-    def check_document_files(self, entry, name):  # * Checks all Document Files
+    def check_document_files(self, entry, name):  
         for documents_extension in document_extensions:
             if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
                 move_file(dest_dir_documents, entry, name)
                 logging.info(f"Moved document file: {name}")
 
 
-# ! NO NEED TO CHANGE BELOW CODE
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
